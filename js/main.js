0e11019ffbcccaf36a8be8581c4c0174fdea6eb9
Alberto Marin <albertomr84@gmail.com>
2017-04-08 18:36:46 +0100

    Fix picking base color problem on mobile

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ const inputs = document.querySelectorAll('.controls input');
 function handleUpdate() {
   // the suffix is for the data-sizing that has 'px' already set
   // or nothing is because we'd get undefined for color
-  const suffix = this.dataset.sizing || '';
+  const suffix = this.dataset.sizing;
   // console.log(suffix);
   // name is the property of the input
   document.documentElement.style.setProperty(`--${(this.name)}`, this.value + suffix);

d34de1fb7b5badf87218ca9023ad8f314763a54f
Alberto Marin <albertomr84@gmail.com>
2017-04-08 15:40:13 +0100

    Add style and functions

---
+++ js/main.js
@@ -0,0 +1,14 @@
+
+const inputs = document.querySelectorAll('.controls input');
+
+function handleUpdate() {
+  // the suffix is for the data-sizing that has 'px' already set
+  // or nothing is because we'd get undefined for color
+  const suffix = this.dataset.sizing || '';
+  // console.log(suffix);
+  // name is the property of the input
+  document.documentElement.style.setProperty(`--${(this.name)}`, this.value + suffix);
+}
+
+inputs.forEach(input => input.addEventListener('change', handleUpdate));
+inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
